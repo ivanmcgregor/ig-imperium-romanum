3756ad1f2ba432283ca9ce6e70b0a3b645e5b8f3
Fabian Piel <fabianpiel@de.ibm.com>
2020-04-02 22:59:34 +0200

    sort images and add member view

--- pages/mitglieder/index.vue
+++ pages/mitglieder/index.vue
@@ -1,10 +1,81 @@
 <template>
-  <span>Platz für Mitglieder</span>
+  <div>
+    <h1>Unsere Mitglieder</h1>
+    <b-row
+      v-for="member in members"
+      :key="member.key"
+      cols="1"
+      cols-sm="2"
+      cols-md="3"
+    >
+      <b-col>
+        <b-img :src="require(`assets/mitglieder/PB-${member.img}.jpg`)" />
+      </b-col>
+      <b-col>
+        <h3>{{ member.name }}</h3>
+        <p>{{ member.text }}</p>
+      </b-col>
+    </b-row>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexVue'
+  name: 'IndexVue',
+  data: () => ({
+    members: [
+      {
+        key: 'nico',
+        img: 'Nico',
+        name: 'Optio Nico',
+        text:
+          'Man munkelt er würde mit seinem Helmbusch eine Kriegsnarbe verstecken.'
+      },
+      {
+        key: 'fabian',
+        img: 'Fabian',
+        name: 'Skriptorius Fabius',
+        text:
+          'Behauptungen nach ist er der einzige der Truppe, der schreiben kann.'
+      },
+      {
+        key: 'manu',
+        img: 'Manu',
+        name: 'Legionär Manus',
+        text: 'Sie nannten ihn Legionär Hand.'
+      },
+      {
+        key: 'thomas',
+        img: 'Thomas',
+        name: 'Legionär Thomasus',
+        text: 'Lorem ipsum Thomasus textus.'
+      },
+      {
+        key: 'franzi',
+        img: 'Franzi',
+        name: 'Bürgerin Franziska',
+        text: 'Lorem ipsum Franziska textus.'
+      },
+      {
+        key: 'andre',
+        img: 'Andre',
+        name: 'Legionär Andreus',
+        text: 'Lorem ipsum Andreus textus.'
+      },
+      {
+        key: 'Max',
+        img: 'Max',
+        name: 'Legionär Maximilius',
+        text: 'Lorem ipsum Maximilius textus.'
+      },
+      {
+        key: 'Lars',
+        img: 'Lars',
+        name: 'Signifer Lars',
+        text: 'Lorem ipsum Lars textus.'
+      }
+    ]
+  })
 }
 </script>
 
