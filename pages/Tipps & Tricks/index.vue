6d6425fb1272c07396c3981c8ff133fdef3bf930
Fabian Piel <fabianpiel@de.ibm.com>
2020-04-16 23:47:13 +0200

    add Anschaffungsratgeber
    This version does not include the BG styling yet.

--- pages/Tipps & Tricks/index.vue
+++ pages/Tipps & Tricks/index.vue
@@ -4,7 +4,24 @@
       <h2 class="mb-5">Tipps und Tricks</h2>
       <b-row class="mb-4">
         <b-col offset-md="1" md="10">
-          Text für T&T
+          <p>
+            Salvete Rekruten und Neueinsteiger ins Römerhobby. In diesem Bereich
+            befindet sich der von unserer Gruppe erarbeitete Wissensfundus. So
+            haben wir beispielsweise einen bebilderten Anschaffungsratgeber.
+            Dieser hilft, einen Überblick zu gewinnen, was welche Gegenstände in
+            welcher Reihenfolge angeschafft werden sollten.
+          </p>
+          <button-wrapper to="/Tipps & Tricks/anschaffungsratgeber"
+            >Zum Anschaffungsratgeber</button-wrapper
+          >
+          <p>
+            Bei vielen Gegenständen, die man als Römer braucht, lohnt es sich,
+            diese selbst herzustellen. Ganz vorne mit dabei ist hier die Tunika
+            zu nennen, aber auch komplexere Gegenstände wurden von unserer
+            Gruppe bereits erstellt. Zu vielen Projekten gibt es auch
+            Anleitungen, die dazu einladen sollen, sich selbst daran zu
+            versuchen.
+          </p>
         </b-col>
       </b-row>
     </Intro>
@@ -12,11 +29,12 @@
 </template>
 
 <script>
+import ButtonWrapper from '~/components/ButtonWrapper'
 import Intro from '~/components/intro'
 
 export default {
   name: 'TippsUndTricks',
-  components: { Intro }
+  components: { ButtonWrapper, Intro }
 }
 </script>
 
